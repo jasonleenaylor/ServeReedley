73d470fb0c4deb81d36c4f67c52abd8eec44f6a0
Jason Naylor <jason_naylor@sil.org>
2021-10-15 15:56:42 -0700

    Add users and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Serve Reedley CRN Homepage.</p>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
