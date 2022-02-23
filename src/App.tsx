aba1bd06dc02e4d497ea25b5b82e762ec15aa943
Jason Naylor <jason_naylor@sil.org>
2022-02-23 13:53:14 -0800

    Update dependencies for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { LocalizeProvider } from "react-localize-redux";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { NeedRequestForm } from "./needRequestForm";
 import NeedRequestTable from "./NeedRequestTable";
@@ -13,45 +11,43 @@ export default function App() {
   const { t } = useTranslation();
   return (
     <ThemeProvider theme={theme}>
-      <LocalizeProvider>
-        <Router>
-          <div style={{ width: "100%" }}>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">{t("home")}</Link>
-                </li>
-                <li>
-                  <Link to="/request-need">Need Help?</Link>
-                </li>
-                <li>
-                  <Link to="/requests">View Current Needs</Link>
-                </li>
-                <li>
-                  <Link to="/need-submitted">Form Completed</Link>
-                </li>
-              </ul>
-            </nav>
+      <Router>
+        <div style={{ width: "100%" }}>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">{t("home")}</Link>
+              </li>
+              <li>
+                <Link to="/request-need">Need Help?</Link>
+              </li>
+              <li>
+                <Link to="/requests">View Current Needs</Link>
+              </li>
+              <li>
+                <Link to="/need-submitted">Form Completed</Link>
+              </li>
+            </ul>
+          </nav>
 
-            {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/request-need">
-                <NeedRequestForm t={t} />
-              </Route>
-              <Route path="/need-submitted">
-                <NeedSubmitted />
-              </Route>
-              <Route path="/requests">
-                <NeedRequestTable t={t} />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </LocalizeProvider>
+          <Switch>
+            <Route path="/request-need">
+              <NeedRequestForm t={t} />
+            </Route>
+            <Route path="/need-submitted">
+              <NeedSubmitted />
+            </Route>
+            <Route path="/requests">
+              <NeedRequestTable t={t} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
