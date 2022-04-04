e7a3582905b92c4fa3939c9434355b92947439c5
Jason Naylor <jason_naylor@sil.org>
2022-04-04 13:52:20 -0700

    Remove testing header
    * Remove header with links
    * Use smaller size for image on form submitted page

--- src/NeedSubmitted.tsx
+++ src/NeedSubmitted.tsx
@@ -1,10 +1,12 @@
-import { Card, CardMedia, Typography } from "@material-ui/core";
+import { Card, CardMedia, Typography, useMediaQuery } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { cardStyle } from "./needRequestForm";
 import logo from "./ServeReedleyLogo.webp";
+import theme from "./theme";
 
 function NeedSubmitted() {
   const { t } = useTranslation();
+  let small = useMediaQuery(theme.breakpoints.up("sm"));
   return (
     <Card style={cardStyle}>
       <CardMedia
@@ -12,7 +14,11 @@ function NeedSubmitted() {
         height="140"
         image={logo}
         alt="Serve Reedley"
-        style={{ width: "590px", height: "114px", justifyContent: "left" }}
+        style={{
+          width: !small ? "295px" : "590px",
+          height: !small ? "57px" : "114px",
+          justifyContent: "left",
+        }}
       />{" "}
       <div className="about">
         <Typography>{t("form_complete_note")}</Typography>
