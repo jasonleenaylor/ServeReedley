a1cc64ff3715e9068c7daaae939cfaaf97ccb428
Jason Naylor <jason.naylor@gmail.com>
2025-09-12 15:30:39 -0700

    Switch findlatlong to use environment variable for geocoding api

--- amplify/backend/function/findlatlong/src/index.js
+++ amplify/backend/function/findlatlong/src/index.js
@@ -1,40 +1,18 @@
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
 import NodeGeocoder from "node-geocoder";
  /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 export const handler = async (event) => {
-  const secret_name = "geocoding-api-key";
- 
-  const client = new SecretsManagerClient({
-    region: "us-west-1",
-  });
- 
-  let response;
- 
-  try {
-    response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-    );
-  } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
+  const apiKey = process.env.GEOCODING_API_KEY;
+  if (!apiKey) {
+    throw new Error("Missing GEOCODING_API_KEY env var");
   }
- 
-  const secret = response.SecretString;
   const { address } = event;
   const options = {
     provider: 'google',
  
     // Optional depending on the providers
-    apiKey: JSON.parse(secret)['key']
+    apiKey: apiKey
   };
   const geocoder = NodeGeocoder(options);
   const geoResponse = await geocoder.geocode(address);
